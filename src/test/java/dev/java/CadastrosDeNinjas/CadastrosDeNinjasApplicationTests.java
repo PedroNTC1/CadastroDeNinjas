52d68a005c83b447bfa230924c3e812bf2dc93b6
Pedro Sérgio <psmlima.cic@uesc.br>
2026-02-26 17:49:28 -0300

    first commit

---
+++ src/test/java/dev/java/CadastrosDeNinjas/CadastrosDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastrosDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
