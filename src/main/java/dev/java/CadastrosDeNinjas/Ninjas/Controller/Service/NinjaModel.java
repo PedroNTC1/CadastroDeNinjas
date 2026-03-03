2878e0110ce5283cf3119a7cf9c0657e72ee6001
Pedro Sérgio <psmlima.cic@uesc.br>
2026-03-03 19:27:51 -0300

    feat: Coloca o Lombok na classe para abstraçao

--- src/main/java/dev/java/CadastrosDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java/CadastrosDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,66 +2,34 @@ package dev.java.CadastrosDeNinjas.Ninjas.Controller.Service;
 
 import dev.java.CadastrosDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 //Transforma uma classe em uma entidade do banco de dados
 @Entity
 @Table(name = "tb_Cadastro_de_ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String nome;
-
     private String email;
-
     private int idade;
+
 
     //Um ninja tem uma unica missao
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-
-
-
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
     @Override
     public String toString() {
